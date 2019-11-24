46329ba620c340aa2afa6d3dda9626aad319cfb4
Pulkit <aggarwalpulkit596@gmail.com>
2019-11-24 15:22:57 +0530

    Add Listeners

--- app/src/main/java/com/codingblocks/chatapp/MainActivity.kt
+++ app/src/main/java/com/codingblocks/chatapp/MainActivity.kt
@@ -17,7 +17,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         loginBtn.setOnClickListener {
-            auth.createUserWithEmailAndPassword(email.text.toString(),pass.text.toString())
+            auth.createUserWithEmailAndPassword(email.text.toString(), pass.text.toString())
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Account Created Successfully", Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener {
+                    if (it.localizedMessage.contains("already", true)) {
+                        signIn(email.text.toString(), pass.text.toString())
+                    } else {
+                        Toast.makeText(this, "${it.localizedMessage}", Toast.LENGTH_LONG).show()
+                    }
+
+                }
         }
+    }
+
+    private fun signIn(email: String, pass: String) {
+            auth.signInWithEmailAndPassword(email,pass)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Logged In Successfully", Toast.LENGTH_LONG).show()
+                }.addOnFailureListener {
+                    Toast.makeText(this, "${it.localizedMessage}", Toast.LENGTH_LONG).show()
+                }
     }
 }
